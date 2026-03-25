53f620e6c7f8014f492bdc72999c183f9a11d8a9
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-03-25 08:08:01 -0500

    Added Matcher in tests

--- src/test/java/com/noobsmoke/OrderServiceTest.java
+++ src/test/java/com/noobsmoke/OrderServiceTest.java
@@ -10,10 +10,12 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.Map;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
@@ -35,6 +37,23 @@ public class OrderServiceTest {
         boolean answer = underTest.processOrder(bigDecimal);
         verify(stripePaymentProcessor).charge(bigDecimal);
         assertTrue(answer);
+    }
+
+    @Test
+    void testAnyMatcher() {
+        Map<String, String> mockMap = mock();
+        when(mockMap.get(anyString())).thenReturn("Hello");
+        assertThat(mockMap.get("Vegeta")).isEqualTo("Hello");
+        verify(mockMap, times(1)).get(anyString());
+    }
+
+    @Test
+    void testEqMatcher() {
+        Map<String, String> mockMap = mock();
+        when(mockMap.put(anyString(), eq("Jackie Chan"))).thenReturn("Hello");
+        ;
+        assertThat(mockMap.put("Hello", "Jackie Chan")).isEqualTo("Hello");
+        verify(mockMap).put("Hello", "Jackie Chan");
     }
 
 }
