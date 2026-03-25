79db3e7a77ce2e3c0148866fa0bb7c63f1a93d93
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-03-25 07:41:36 -0500

    Implemented AutoCloseable

--- src/test/java/com/noobsmoke/OrderServiceTest.java
+++ src/test/java/com/noobsmoke/OrderServiceTest.java
@@ -1,16 +1,41 @@
 package com.noobsmoke;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class OrderServiceTest {
 
     private OrderService underTest;
     @Mock
     private PaymentProcessor stripePaymentProcessor;
+    private AutoCloseable autoCloseable;
 
     @BeforeEach
     void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
         underTest = new OrderService(stripePaymentProcessor);
+    }
+
+    @Test
+    void amountIsChargedTest() {
+        BigDecimal bigDecimal = BigDecimal.valueOf(1000.12);
+        when(stripePaymentProcessor.charge(bigDecimal)).thenReturn(true);
+        boolean answer = underTest.processOrder(bigDecimal);
+        verify(stripePaymentProcessor).charge(bigDecimal);
+        assertTrue(answer);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
 }
